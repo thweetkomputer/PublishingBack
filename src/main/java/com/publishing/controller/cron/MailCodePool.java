b517158611900dbd0d8203ba7f371753dca29914
wxie7 <601862884@qq.com>
2021-05-19 15:37:08 +0800

    添加邮箱验证码池，定时清楚无效验证码

---
+++ src/main/java/com/publishing/controller/cron/MailCodePool.java
@@ -0,0 +1,50 @@
+package com.publishing.controller.cron;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class MailCodePool {
+    private static final HashMap<String, Properties> mailPool = new HashMap<>(); // email -> properties
+    private static final int NUM_THREADS = 50;
+    private static final ScheduledExecutorService pool = Executors.newScheduledThreadPool(NUM_THREADS);
+
+    public static void putMailCode(String email, int code) {
+        Properties properties = new Properties();
+        properties.put("email", email);
+        properties.put("code", code);
+        properties.put("time", new Date());
+        mailPool.put(email, properties);
+        pool.submit(new ClearDaemon(email));
+        pool.schedule(new ClearDaemon(email), 5, TimeUnit.SECONDS);
+    }
+
+    public static Integer getMailCode(String email) {
+        Properties properties = mailPool.remove(email);
+        if (properties == null)
+            return null;
+        return (Integer) properties.get("code");
+    }
+
+    private static void removeMailCode(String email) {
+        mailPool.remove(email);
+    }
+
+    private static class ClearDaemon implements Runnable {
+        private final String email;
+
+        public ClearDaemon(String email) {
+            this.email = email;
+        }
+
+        @Override
+        public void run() {
+            removeMailCode(email);
+        }
+    }
+}
