364d99a4aaa11d4dc2d1381de4827c78b9f9f71a
thweetkomputer <2320092610@qq.com>
2021-06-07 20:10:46 +0800

    完成了出版文章的模块

--- src/main/java/com/publishing/controller/PassageController.java
+++ src/main/java/com/publishing/controller/PassageController.java
@@ -3,6 +3,9 @@ package com.publishing.controller;
 
 import cn.hutool.core.map.MapUtil;
 import com.publishing.common.lang.Result;
+import com.publishing.entity.Notice;
+import com.publishing.entity.Passage;
+import com.publishing.service.NoticeService;
 import com.publishing.service.PassageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -29,16 +32,28 @@ public class PassageController {
     @Autowired
     private PassageService passageService;
 
+    @Autowired
+    private NoticeService noticeService;
+
     @Value("${storage.pathname}")
     private String pathname;
 
     @RequestMapping("/getPassage")
     public Result getPassage(@RequestParam("article_title") Long id) throws IOException {
-        File file = new File(pathname + "xuqiuguigeshuomingshu.pdf");
+        File file = new File(pathname + id + ".pdf");
         FileInputStream inputStream = new FileInputStream(file);
         return Result.succeed(MapUtil.builder()
-        .put("description", passageService.getById(id).getDescription())
-        .put("title", passageService.getById(id).getTitle())
-        .map());
+                .put("description", passageService.getById(id).getDescription())
+                .put("title", passageService.getById(id).getTitle())
+                .map());
+    }
+
+    @RequestMapping("/publishPassage")
+    public Result publishPassage(@RequestParam("article_id") Long id) {
+        Passage passage = passageService.getById(id);
+        passage.setPublished(1);
+        passageService.updateById(passage);
+        noticeService.save(new Notice(passage.getWriterId(), "您的文章Id为"+id+"，题目为 "+ passage.getTitle()+ " 的文章已经出版啦！"));
+        return Result.succeed(200, "出版成功", null);
     }
 }
