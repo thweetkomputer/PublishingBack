0f64ffbd3c2e7c687fb70f132f59a64d33169a67
thweetkomputer <2320092610@qq.com>
2021-06-09 01:08:10 +0800

    消息中心模块（未测试）

--- src/main/java/com/publishing/controller/NoticeController.java
+++ src/main/java/com/publishing/controller/NoticeController.java
@@ -1,9 +1,18 @@
 package com.publishing.controller;
 
 
+import cn.hutool.core.map.MapUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.publishing.common.lang.Result;
+import com.publishing.entity.Notice;
+import com.publishing.service.NoticeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -14,7 +23,26 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2021-05-13
  */
 @RestController
-@RequestMapping("/notice")
 public class NoticeController {
+    @Autowired
+    private NoticeService noticeService;
 
+    @RequestMapping("/getNotice")
+    public Result getNotice(@RequestParam("page") int page,
+                            @RequestParam("pageSize") int pageSize,
+                            @RequestParam("user_id") Long userId) {
+        List<Notice> noticeList = noticeService.list(new QueryWrapper<Notice>().eq("receiver_id", userId));
+        int startPage = (page - 1) * pageSize;
+        return Result.succeed(MapUtil.builder()
+                .put("notice_list", noticeList.subList(startPage, Math.min(startPage + pageSize, noticeList.size())))
+                .put("total_num", noticeList.size())
+                .map());
+    }
+
+    @RequestMapping("/readNotice")
+    public Result readNotice(@RequestParam("notice_id") Long noticeId) {
+        Notice notice = noticeService.getById(noticeId);
+        notice.setHasRead(1);
+        return Result.succeed(null);
+    }
 }
